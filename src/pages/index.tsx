7dee98dfb2ee823d3510baf7beeaf254d6a2176d
Guolin <guolinf@gmail.com>
2022-04-16 20:10:20 +0000

    openstreetmap

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+// imports
+import React from "react";
+
+import dynamic from "next/dynamic";
+
+export default function Home() {
+  // needed to make the Leaflet map render correctly
+  const MapWithNoSSR = dynamic(() => import("../components/Map"), {
+    ssr: false,
+  });
+  // logic to transform data into the items needed to pass to the map
+  return (
+    <div>
+      <MapWithNoSSR />
+    </div>
+  );
+}
