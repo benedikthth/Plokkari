1114576892b8ff62d44105f411ac7f347d2233b5
benedikt <benedikthth@ru.is>
2022-04-20 15:03:14 +0000

    Draw polygons

--- src/components/drawHex/DrawHex.tsx
+++ src/components/drawHex/DrawHex.tsx
@@ -8,7 +8,8 @@ function DrawHex() {
     const h3 = require("h3-js");
     const [boundedHex, setBoundedHex] = useState(null);
     let number = 0;
-    const [data, setData] = useState(null);  
+    const [data, setData] = useState([]);  
+
     const draw={
       circlemarker: false,
       marker: false,
@@ -28,20 +29,22 @@ function DrawHex() {
     const _onCreated = (e) => {
       //console.log(h3.polyfill(e.layer._latlngs, 10))
       const geometry = e.layer.toGeoJSON().geometry.coordinates;
-      const data = h3.polyfill(geometry, 10);
-      const coordinates = h3.h3SetToMultiPolygon(data, false);
+      const data = h3.polyfill(geometry, 9);
+      const coordinates = data.map(x=>h3.h3SetToMultiPolygon([x], true));
+      // const coordinates = h3.h3SetToMultiPolygon(data, true);
       console.log(coordinates)
       setData(coordinates)
-      var bh = [];
-      coordinates.forEach((data) => {
-        bh.push(<Polygon color={'green'} key={number = number +1} positions={data}/>)
-      })
-      setBoundedHex(bh)
+      // var bh = [];
+      // coordinates.forEach((data) => {
+      //   bh.push(<Polygon fill={true} color={'green'} key={number = number +1} positions={data}/>)
+      // })
+      // setBoundedHex(bh)
     }
 
 
     return (
       <div>
+      {data.map(coordinateSet=> <Polygon key={data.indexOf(coordinateSet)} color="green" positions={coordinateSet}/>) }
         <FeatureGroup ref={featureGroupRef}>
         <EditControl
           position="topleft"
@@ -51,7 +54,6 @@ function DrawHex() {
           edit={edit}
         />
       </FeatureGroup>
-      {boundedHex}
     </div>
     )
 }
