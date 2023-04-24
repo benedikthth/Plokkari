ea42e052a554129c08a353526bcedbe0c9e1ecc9
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2023-04-24 22:55:36 +0000

    Swtich to dirty/clean

--- src/components/congrazBox/CongrazBox.tsx
+++ src/components/congrazBox/CongrazBox.tsx
@@ -35,7 +35,7 @@ function CongrazBox({data, eraseData}) {
             autoplay: true,
             animationData: location
           });
-            fetch(`${process.env.NEXT_PUBLIC_API_URL}/Trash/Trash`, {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/Trash/Clean`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
